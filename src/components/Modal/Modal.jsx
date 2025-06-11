719f7b6458458458846aa7720e08d2e9deb46f7d
Braden Kemp Jones <bradenjonesgt@meta.com>
2025-06-11 15:04:17 -0700

    Modal working, adding css and minutes chnages, still need to fix loading for search and add sort

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { useState } from "react";
+import { fetchMovies } from "../../api/fetchMovies";
+import { use } from "react";
+import { useEffect } from "react";
+import "./Modal.css";
+
+
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+
+export default function Modal({movieId}){
+    const [isOpen,setOpen] = useState(false);
+    const [movieData, setMovieData] = useState([])
+    const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
+
+        //function to get json data for Modal
+    const returnDetails = async () => {
+        const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY}`
+        }
+        };
+
+        try{
+            const response = await fetch(url,options);
+            const data = await response.json();
+            console.log(data.genres)
+            setMovieData(data);
+        } catch(error){
+            console.error('failed to fetch movies',error);
+            throw error;
+        }
+    };
+
+    // Use useEffect to fetch data and open modal when data is available
+    useEffect(() => {
+        if (movieId) {
+        returnDetails();
+        }
+    }, [movieId]);
+    // Open modal when movieData is set
+    useEffect(() => {
+        if (movieData) {
+        setOpen(true);
+        }
+    }, [movieData]);
+    // Function to close the modal
+    const closeModal = () => {
+        setOpen(false);
+    };
+        
+
+
+    //what we are returning after fetching, title, image, releasedate,overiew,genres
+    //movieData.title for title
+    //`${BASEURL}${movieData.backdrop_path}` for image
+    //movieData.release_date for release date
+    //movieData.overview for overview
+    //genres is an array for for each
+    //only want to return or show display if the click button is true
+    return (
+        <div>
+            {isOpen && movieData && (
+                <div className="modal">
+                    <div className ="modal-background">
+                    <h2 className="movie-title">{movieData.title}</h2>
+                    <img className="image-photo" src={`${BASE_URL}${movieData.backdrop_path}`} alt={movieData.title} />
+                    <p className="release-date">Release Date: {movieData.release_date}</p>
+                    <p className="overview">Overview: {movieData.overview}</p>
+                    <p className= "genre-list">Genres:</p>
+                    <ul className="genre-text">
+                        {(movieData.genres)?.map((genre) => (// shoutout StackOVerFLOW, found a fix to the issue of map in seconds
+                            <li key={genre.id}>{genre.name}</li> 
+                        ))} {/* Append list items using map*/}
+                    </ul>
+                    <button className="close-button" onClick = {closeModal}>Close</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+        
+
+
+// <ul className="genre-text">
+                    //{movieData.genres.map((genre, index) => (
+                       // <li key={index}>{genre.name}</li>
+                    //))}
+                    //</ul>
+
+
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import './MovieCard.css';
-
+import Modal from "../Modal/Modal.jsx";
+import { useState } from "react";
 const BASE_URL = "https://image.tmdb.org/t/p/w500";
 
+
+
 const MovieCard = ({ data }) => {
+
+    const [isModalOpen, setModalOpen] = useState(false);
+    const openModal = () => {
+        setModalOpen(true);
+    };
+    // Function to close the modal
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
     return (
-        <div className="movie-card">
-            <img 
-                className="movie-image" 
-                src={`${BASE_URL}${data.poster_path}`} 
-                alt={data.title}
-            />
-            <div className="movie-box">
-                <h3 className="title">{data.original_title}</h3>
-                <p className="votes">Voting Average: {data.vote_average}</p>
+        <>
+            <div className="movie-card" onClick={openModal}>
+                <img
+                    className="movie-image"
+                    src={`${BASE_URL}${data.poster_path}`}
+                    alt={data.title}
+                />
+                <div className="movie-box">
+                    <h3 className="title">{data.original_title}</h3>
+                    <p className="votes">Voting Average: {data.vote_average}</p>
+                </div>
             </div>
-        </div>
+            {isModalOpen && (
+                <Modal movieId={data.id} onClose={closeModal} />
+            )}
+        </>
     );
 };
 
